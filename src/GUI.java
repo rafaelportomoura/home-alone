a35c184adbd3f99dbf53981bc3046fec65ac3ab3
Gláucio Martins Lima <60272935+glaucio798@users.noreply.github.com>
2021-11-09 22:27:21 -0300

    Update GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -1,9 +1,7 @@
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.ComponentOrientation;
+
 public class GUI{
     private JFrame janela;
     // Sul
